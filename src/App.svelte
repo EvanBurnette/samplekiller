68dc2ed82f81279efe8dd13458a9e7add2457d0f
Evan Burnette <EvanBurnette@users.noreply.github.com>
2021-04-30 02:59:11 -0400

    Converting object to array for sorting but it breaks stuff

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,23 @@
 	// import {onMount} from 'svelte';
 	let fileInput = {'files': []}
 	let samples = []
+	// let audioSrc = '#'
+	const reader = new FileReader()
 	let handleChange = () => {
 		samples = Array.from(fileInput.files).sort((a, b) => a.size - b.size)
+		// audioSrc = reader.readAsDataURL(fileInput.files[0])
+		console.log(fileInput.files)
 	}
 </script>
 
 <main>
 	<input multiple type="file" id="fileInput" on:change={handleChange} bind:this={fileInput}>
-	{#each samples as sample}
-	<p>{sample.size}</p>
+	<!-- svelte-ignore a11y-media-has-caption -->
+	<!-- <audio src={audioSrc} controls></audio> -->
+	<ul>
+	{#each samples as sample, index}
+	<li>{sample.name} size:{sample.size} <audio src={undefined} controls></audio></li>
+	
 	{/each}
+</ul>
 </main>
