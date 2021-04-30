1980003f875a29b1c88d3ce465babc1ff3d66c9b
Evan Burnette <EvanBurnette@users.noreply.github.com>
2021-04-30 02:00:48 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	// import {onMount} from 'svelte';
+	let fileInput = {'files': []}
+	let samples = []
+	let handleChange = () => {
+		samples = Array.from(fileInput.files).sort((a, b) => a.size - b.size)
+	}
+</script>
+
+<main>
+	<input multiple type="file" id="fileInput" on:change={handleChange} bind:this={fileInput}>
+	{#each samples as sample}
+	<p>{sample.size}</p>
+	{/each}
+</main>
